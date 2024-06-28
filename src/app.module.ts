42b03d8a7c14d9dc2731c9ff5bd3464e06c07ee9
Guddi Yadav <guddiyadav991@gmail.com>
2024-06-28 16:12:20 +0530

    Create Users CRUD operation using command

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@
 
 import { Module } from '@nestjs/common';
 import { BookModule } from './book/book.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BookModule],
+  imports: [BookModule, UsersModule],
   controllers: [],
   providers: [],
 })
