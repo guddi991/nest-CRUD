2478b9a9803e54a8bc8f728b83bcdf524efc1459
Guddi Yadav <guddiyadav991@gmail.com>
2024-06-30 19:57:19 +0530

    global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+/* eslint-disable prettier/prettier */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NextFunction, Request, Response } from 'express';
+
+// global based functional middleware => works with every request.
+
+
+function globalMiddleWareOne(req : Request, res : Response, next : NextFunction){
+  console.log("global middleware 1 ")
+  next();
+}
+
+// middleware2
+
+function globalMiddleWareTwo(req : Request, res : Response, next : NextFunction){
+  console.log("global middleware 2");
+  next()
+}
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(globalMiddleWareOne);
+  app.use(globalMiddleWareTwo);
   await app.listen(3000);
 }
 bootstrap();
