ffe47cbfb710b6633a32adfd8f77c4deb46f8462
Guddi Yadav <guddiyadav991@gmail.com>
2024-06-30 20:16:08 +0530

    usecase of middleware

--- src/book/book.middleware.ts
+++ src/book/book.middleware.ts
@@ -6,6 +6,13 @@ import { Request, Response, NextFunction } from "express";
 export class BookMiddleWare implements NestMiddleware{
     
     use(req : Request, res : Response, next: NextFunction){
+        let protocol = req.protocol; // http https
+        let host = req.get('host') // localhost
+        let url = req.originalUrl;
+        let method = req.method;
+        let date = new Date().toDateString();
+
+        console.log(protocol + "://"+host+url+" "+method+ date)
         console.log("Class Based Middleware for book.")
         next();
     }
